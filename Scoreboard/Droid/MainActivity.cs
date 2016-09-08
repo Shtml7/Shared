3a1c207d7c02895a7536dd76102023be14b53646
Hoeks, Maikel <maikelhoeks@hotmail.com>
2016-09-08 10:22:26 +0200

    added files

--- Scoreboard/Droid/MainActivity.cs
+++ Scoreboard/Droid/MainActivity.cs
@@ -1,28 +1,33 @@
-﻿using Android.App;
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Scoreboard;
 
 namespace Scoreboard.Droid
 {
-	[Activity(Label = "Scoreboard", MainLauncher = true, Icon = "@mipmap/icon")]
+	[Activity (Label = "Scoreboard.Droid", MainLauncher = true)]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+        protected async override void OnCreate(Bundle bundle)
+        {
+            MyClass clas = new MyClass();
+            UserCall call = new UserCall();
+            base.OnCreate(bundle);
 
-		protected override void OnCreate(Bundle savedInstanceState)
-		{
-			base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
 
-			// Set our view from the "main" layout resource
-			SetContentView(Resource.Layout.Main);
-
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
-
-			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
-		}
-	}
+            // Our code will go here
+            EditText text = FindViewById<EditText>(Resource.Id.text);
+            User u = await call.getUser();
+            text.Text = u.name;
+        }
+    }
 }
 
 
---
+++ Scoreboard/UserCall.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Scoreboard
+{
+    public class UserCall
+    {
+        private static string baseUrl = "http://77.175.219.85:9090";
+
+        public async Task<User> getUser()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("/scoreboard/api/users/test");
+                User u = null;
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    System.Diagnostics.Debug.WriteLine("RESPONSE: " + jsonResponse);
+                    u = JsonConvert.DeserializeObject<User>(jsonResponse);
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine("Something went wrong with the API call. Status code: " + response.StatusCode);
+                    throw new WebException("Could not reach the server. Status code: " + response.StatusCode);
+                }
+                return u;
+            }
+        }
+    }
+}
