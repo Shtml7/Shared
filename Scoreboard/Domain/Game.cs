52b168b5e559c7861487d7cdb8b285d75c1e1932
Hoeks, Maikel <maikelhoeks@hotmail.com>
2016-09-12 12:24:05 +0200

    added domain classes

---
+++ Scoreboard/Domain/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scoreboard.domain
+{
+    class Game
+    {
+        private long id { get; set; }
+        private User owner { get; set; }
+        private Boolean isActive { get; set; }
+        private Team team1 { get; set; }
+        private Team team2 { get; set; }
+
+
+    }
+}
---
+++ Scoreboard/Domain/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scoreboard.domain
+{
+    class Team
+    {
+        private long id { get; set; }
+        private User player1 { get; set; }
+        private User player2 { get; set; }
+        private int score { get; set; }
+    }
+}
